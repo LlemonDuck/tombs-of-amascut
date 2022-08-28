4e425fef45b0f7bad97a94eb17785c538a9155e0
TheStonedTurtle <mgoodwin559@gmail.com>
2022-08-27 18:51:05 -0700

    Add Invocation Screenshot component
    Add authors to repo LICENSE
    
    Add Invocation Screenshot to readme
    
    little cleanup

--- src/main/java/com/duckblade/osrs/toa/TombsOfAmascutConfig.java
+++ src/main/java/com/duckblade/osrs/toa/TombsOfAmascutConfig.java
@@ -3,6 +3,7 @@ package com.duckblade.osrs.toa;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.ConfigSection;
 
 @ConfigGroup(TombsOfAmascutConfig.CONFIG_GROUP)
 public interface TombsOfAmascutConfig extends Config
@@ -32,4 +33,47 @@ public interface TombsOfAmascutConfig extends Config
 		return true;
 	}
 
+	@ConfigSection(
+		name = "Invocation Screenshot",
+		description = "All config options related to the Invocation Screenshot functionality",
+		closedByDefault = true,
+		position = 200
+	)
+	String SECTION_INVOCATION_SCREENSHOT = "invocationScreenshotSection";
+
+	@ConfigItem(
+		keyName = "invocationScreenshotEnable",
+		name = "Enable Screenshot button",
+		description = "Adds a button to the ToA Invocation interface that will copy all invocations as an image to your system clipboard",
+		section = SECTION_INVOCATION_SCREENSHOT,
+		position = 201
+	)
+	default boolean invocationScreenshotEnable()
+	{
+		return true;
+	}
+
+	@ConfigItem(
+		keyName = "showRewardsSection",
+		name = "Show Rewards Section",
+		description = "<html>Should the rewards section be included<br/>(requires the Reward button to be selected within the interface)</html>",
+		section = SECTION_INVOCATION_SCREENSHOT,
+		position = 202
+	)
+	default boolean showRewardsSection()
+	{
+		return true;
+	}
+
+	@ConfigItem(
+		keyName = "useResourcePack",
+		name = "Use Resource Pack",
+		description = "Use Resource Pack Theme for screenshot background",
+		section = SECTION_INVOCATION_SCREENSHOT,
+		position = 203
+	)
+	default boolean useResourcePack()
+	{
+		return true;
+	}
 }
