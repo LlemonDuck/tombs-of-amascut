4f4a56ffec21007b6086f12bcddf23936d2e3e76
Paul Norton <napkinorton@gmail.com>
2022-08-27 23:09:15 -0400

    rearrange config

--- src/main/java/com/duckblade/osrs/toa/TombsOfAmascutConfig.java
+++ src/main/java/com/duckblade/osrs/toa/TombsOfAmascutConfig.java
@@ -35,10 +35,54 @@ public interface TombsOfAmascutConfig extends Config
 	}
 
 	@ConfigSection(
-		name = "Chest Audio",
+		name = "Invocation Screenshot",
+		description = "All config options related to the Invocation Screenshot functionality",
+		closedByDefault = true,
+		position = 100
+	)
+	String SECTION_INVOCATION_SCREENSHOT = "invocationScreenshotSection";
+
+	@ConfigItem(
+		keyName = "invocationScreenshotEnable",
+		name = "Enable Screenshot button",
+		description = "Adds a button to the ToA Invocation interface that will copy all invocations as an image to your system clipboard",
+		section = SECTION_INVOCATION_SCREENSHOT,
+		position = 101
+	)
+	default boolean invocationScreenshotEnable()
+	{
+		return true;
+	}
+
+	@ConfigItem(
+		keyName = "showRewardsSection",
+		name = "Show Rewards Section",
+		description = "<html>Should the rewards section be included<br/>(requires the Reward button to be selected within the interface)</html>",
+		section = SECTION_INVOCATION_SCREENSHOT,
+		position = 102
+	)
+	default boolean showRewardsSection()
+	{
+		return true;
+	}
+
+	@ConfigItem(
+		keyName = "useResourcePack",
+		name = "Use Resource Pack",
+		description = "Use Resource Pack Theme for screenshot background",
+		section = SECTION_INVOCATION_SCREENSHOT,
+		position = 103
+	)
+	default boolean useResourcePack()
+	{
+		return true;
+	}
+
+	@ConfigSection(
+		name = "Custom Purple Sarcophagus Sound",
 		description = "<html>Can play a custom sound file when the purple loot sarcophagus is opened." +
 			"<br/>Custom sound must be provided at ~/.runelite/tombs-of-amascut/toa-chest.wav</html>",
-		position = 100,
+		position = 200,
 		closedByDefault = true
 	)
 	String SECTION_SARCOPHAGUS_SOUND = "sectionSarcophagusSound";
@@ -49,7 +93,7 @@ public interface TombsOfAmascutConfig extends Config
 		description = "<html>Either disables the feature or plays an audio file whenever the purple chest is opened." +
 			"<br/>The custom audio file should be named toa-chest.wav inside the .runelite/tombs-of-amascut folder</html>",
 		section = SECTION_SARCOPHAGUS_SOUND,
-		position = 101
+		position = 201
 	)
 	default boolean chestAudioEnable()
 	{
@@ -65,54 +109,10 @@ public interface TombsOfAmascutConfig extends Config
 		name = "Audio Volume",
 		description = "Adjusts how loud the chest audio is when played. 100 is no change to file volume.",
 		section = SECTION_SARCOPHAGUS_SOUND,
-		position = 102
+		position = 202
 	)
 	default int chestAudioVolume()
 	{
 		return 100;
-	}
-
-	@ConfigSection(
-		name = "Invocation Screenshot",
-		description = "All config options related to the Invocation Screenshot functionality",
-		closedByDefault = true,
-		position = 200
-	)
-	String SECTION_INVOCATION_SCREENSHOT = "invocationScreenshotSection";
-
-	@ConfigItem(
-		keyName = "invocationScreenshotEnable",
-		name = "Enable Screenshot button",
-		description = "Adds a button to the ToA Invocation interface that will copy all invocations as an image to your system clipboard",
-		section = SECTION_INVOCATION_SCREENSHOT,
-		position = 201
-	)
-	default boolean invocationScreenshotEnable()
-	{
-		return true;
-	}
-
-	@ConfigItem(
-		keyName = "showRewardsSection",
-		name = "Show Rewards Section",
-		description = "<html>Should the rewards section be included<br/>(requires the Reward button to be selected within the interface)</html>",
-		section = SECTION_INVOCATION_SCREENSHOT,
-		position = 202
-	)
-	default boolean showRewardsSection()
-	{
-		return true;
-	}
-
-	@ConfigItem(
-		keyName = "useResourcePack",
-		name = "Use Resource Pack",
-		description = "Use Resource Pack Theme for screenshot background",
-		section = SECTION_INVOCATION_SCREENSHOT,
-		position = 203
-	)
-	default boolean useResourcePack()
-	{
-		return true;
 	}
 }
