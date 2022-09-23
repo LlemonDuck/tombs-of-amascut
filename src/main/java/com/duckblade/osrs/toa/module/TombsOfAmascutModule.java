886c5783fe95ccee1247b5cd07b7745f935be498
Paul Norton <napkinorton@gmail.com>
2022-09-23 15:59:10 -0400

    scabaras: addition puzzle solver

--- src/main/java/com/duckblade/osrs/toa/module/TombsOfAmascutModule.java
+++ src/main/java/com/duckblade/osrs/toa/module/TombsOfAmascutModule.java
@@ -4,12 +4,13 @@ import com.duckblade.osrs.toa.TombsOfAmascutConfig;
 import com.duckblade.osrs.toa.features.DepositPickaxeSwap;
 import com.duckblade.osrs.toa.features.InvocationScreenshot;
 import com.duckblade.osrs.toa.features.QuickProceedSwaps;
-import com.duckblade.osrs.toa.features.scabaras.overlay.LightPuzzleSolver;
-import com.duckblade.osrs.toa.features.scabaras.overlay.ScabarasOverlayManager;
-import com.duckblade.osrs.toa.features.targettime.TargetTimeManager;
 import com.duckblade.osrs.toa.features.apmeken.ApmekenWaveInstaller;
 import com.duckblade.osrs.toa.features.invocationpresets.InvocationPresetsManager;
+import com.duckblade.osrs.toa.features.scabaras.overlay.AdditionPuzzleSolver;
+import com.duckblade.osrs.toa.features.scabaras.overlay.LightPuzzleSolver;
+import com.duckblade.osrs.toa.features.scabaras.overlay.ScabarasOverlayManager;
 import com.duckblade.osrs.toa.features.scabaras.panel.ScabarasPanelManager;
+import com.duckblade.osrs.toa.features.targettime.TargetTimeManager;
 import com.duckblade.osrs.toa.util.RaidStateTracker;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
@@ -26,6 +27,7 @@ public class TombsOfAmascutModule extends AbstractModule
 	protected void configure()
 	{
 		Multibinder<PluginLifecycleComponent> lifecycleComponents = Multibinder.newSetBinder(binder(), PluginLifecycleComponent.class);
+		lifecycleComponents.addBinding().to(AdditionPuzzleSolver.class);
 		lifecycleComponents.addBinding().to(ApmekenWaveInstaller.class);
 		lifecycleComponents.addBinding().to(DepositPickaxeSwap.class);
 		lifecycleComponents.addBinding().to(InvocationPresetsManager.class);
