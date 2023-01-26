e6a672eee1856833038f39f34bd50caf4ac2058a
Paul Norton <napkinorton@gmail.com>
2023-01-26 03:07:48 -0500

    updatenotifier: rev4

--- src/main/java/com/duckblade/osrs/toa/features/updatenotifier/UpdateNotifier.java
+++ src/main/java/com/duckblade/osrs/toa/features/updatenotifier/UpdateNotifier.java
@@ -19,7 +19,7 @@ import net.runelite.client.util.ImageUtil;
 public class UpdateNotifier implements PluginLifecycleComponent
 {
 
-	public static final int TARGET_VERSION = 3;
+	public static final int TARGET_VERSION = 4;
 	private static final BufferedImage PANEL_ICON = ImageUtil.loadImageResource(UpdateNotifier.class, "icon.png");
 
 	private final ClientToolbar clientToolbar;
@@ -77,13 +77,18 @@ public class UpdateNotifier implements PluginLifecycleComponent
 	{
 		List<String> updates = new ArrayList<>();
 
+		if (config.updateNotifierLastVersion() < 3)
+		{
+			updates.add("<strong>A bug was fixed that prevented this update panel from appearing. If this is your first time seeing it, some of the below changes might be things you've already been using for a while!</strong>");
+		}
+
 		switch (config.updateNotifierLastVersion())
 		{
 			case 0:
 				updates.add("A set of puzzle solvers for the Path of Scabaras have been added, which will overlay solutions for you.");
 				updates.add("The Path of Het's Deposit-pickaxe swap can now remove the option to enter the next room until you have deposited your pickaxe.");
 				updates.add("Plugin-based invocation presets have been readded, but are disabled by default.");
-				updates.add("You can now add a custom sound to play when the purple loot chest is opened, e.g. the Legend of Zelda chest loot jingle. Check the config descriptions or plugin README for instructions.");
+				updates.add("You can now add a custom sound to play when the purple loot chest is opened, e.g. the Legend of Zelda chest loot jingle. Check the config descriptions or plugin README for instructions (thanks @TheStonedTurtle!).");
 
 			case 1:
 				updates.add("A points tracker has been added to determine points earned in raids, as well as the unique rate from those points. Calculating points for groups requires players to be in a RuneLite party.");
@@ -91,6 +96,13 @@ public class UpdateNotifier implements PluginLifecycleComponent
 			case 2:
 				updates.add("The Path of Het helper will now display an indicator of when the next beam will fire. Click the seal when it is green from one tile away to get an extra damage tick.");
 				updates.add("Some bugs have been fixed with the points tracker. Please continue to report further issues on GitHub!");
+
+			case 3:
+				updates.add("The Quick-Proceed swaps can now be set to NOT_CRONDIS, to allow team synchronization on that puzzle without disabling swaps for the entire raid.");
+				updates.add("HP Orbs can be toggled off with a new config option.");
+				updates.add("The loot chest Bank-all option can be toggled to no longer require two clicks.");
+				updates.add("The loot sarcophagus flame colours can now be recoloured (thanks @rdutta!).");
+				updates.add("The Scabaras addition puzzle routes now use more optimal paths (thanks @PowContent!).");
 		}
 
 		return updates;
