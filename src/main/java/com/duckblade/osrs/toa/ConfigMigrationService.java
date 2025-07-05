ec5826380945a0db8f27d42cbb266d79f3cecdf3
Rhea <napkinorton@gmail.com>
2025-07-04 21:24:40 -0400

    config migration: call deposit box filter

--- src/main/java/com/duckblade/osrs/toa/ConfigMigrationService.java
+++ src/main/java/com/duckblade/osrs/toa/ConfigMigrationService.java
@@ -35,6 +35,7 @@ public class ConfigMigrationService
 		migrateHideHpOrbs();
 		migratePickaxeReminder();
 		migrateScabarasMatchingDisplayMode();
+		migrateDepositBoxFilterString();
 	}
 
 	@VisibleForTesting
